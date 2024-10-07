1193b0e8fac143049e2649dbea7d0688e76a63b3
Sang Won Lee <freerusya@naver.com>
2024-10-07 22:32:49 +0900

    Initial commit

---
+++ src/main/java/com/example/springbootreviews/SpringBootReviewsApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootreviews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootReviewsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootReviewsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springbootreviews/SpringBootReviewsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootreviews;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootReviewsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
