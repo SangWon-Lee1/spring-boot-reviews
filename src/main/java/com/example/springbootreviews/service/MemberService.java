6c415f962a17a30db5e8455ee0c6a9f49cd0eb9d
Sang Won Lee <freerusya@naver.com>
2024-10-10 00:03:51 +0900

    연습

---
+++ src/main/java/com/example/springbootreviews/service/MemberService.java
@@ -0,0 +1,42 @@
+package com.example.springbootreviews.service;
+
+import com.example.springbootreviews.domain.Member;
+import com.example.springbootreviews.repository.MemberRepository;
+import com.example.springbootreviews.repository.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+    private final MemberRepository memberRepository;
+
+    @Autowired
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    // 회원 가입
+    public Long join(Member member) {
+        validateDuplicateMember(member);
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+                        .ifPresent(m -> {
+                            throw new IllegalStateException("이미 존재하는 회원");
+                        });
+    }
+
+    // 전체 회원 조회
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+}
