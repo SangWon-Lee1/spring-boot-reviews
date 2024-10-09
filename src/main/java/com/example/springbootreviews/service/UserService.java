d9857bda0e2a89ea302991d07f643f1b41efa6ef
Sang Won Lee <freerusya@naver.com>
2024-10-09 16:16:09 +0900

    refactor: 수정

--- src/main/java/com/example/springbootreviews/service/UserService.java
+++ src/main/java/com/example/springbootreviews/service/UserService.java
@@ -3,19 +3,14 @@ package com.example.springbootreviews.service;
 import com.example.springbootreviews.entity.User;
 import com.example.springbootreviews.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor // final 필드를 위한 생성자 자동생성
 public class UserService {
     private final UserRepository userRepository;
-    private final PasswordEncoder passwordEncoder;
 
     public User registerUser(User user) {
-        String encodedPassword = passwordEncoder.encode(user.getPassword());
-        user.setPassword(encodedPassword);
-
         return userRepository.save(user);
     }
 }
