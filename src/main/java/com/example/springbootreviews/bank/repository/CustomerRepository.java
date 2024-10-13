e5631b9914f707a2d41199bc1a93174813346671
Sang Won Lee <freerusya@naver.com>
2024-10-13 17:43:35 +0900

    feat: Add account creation function

--- src/main/java/com/example/springbootreviews/bank/repository/CustomerRepository.java
+++ src/main/java/com/example/springbootreviews/bank/repository/CustomerRepository.java
@@ -4,6 +4,9 @@ import com.example.springbootreviews.bank.entity.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
+    Optional<Customer> findByCustomerId(String customerId);
 }
---
+++ src/main/java/com/example/springbootreviews/bank/service/AccountService.java
@@ -0,0 +1,43 @@
+package com.example.springbootreviews.bank.service;
+
+import com.example.springbootreviews.bank.entity.Account;
+import com.example.springbootreviews.bank.entity.Customer;
+import com.example.springbootreviews.bank.model.AccountDTO;
+import com.example.springbootreviews.bank.repository.AccountRepository;
+import com.example.springbootreviews.bank.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AccountService {
+    private AccountRepository accountRepository;
+    private CustomerRepository customerRepository;
+
+    @Autowired
+    AccountService(AccountRepository accountRepository, CustomerRepository customerRepository) {
+        this.accountRepository = accountRepository;
+        this.customerRepository = customerRepository;
+    }
+
+    public AccountDTO createAccount(AccountDTO accountDTO) {
+        Account account = new Account();
+        account.setAccountId(accountDTO.getAccountId());
+        Customer customer = customerRepository.findByCustomerId(accountDTO.getCustomerId())
+                .orElseThrow(() -> new IllegalArgumentException("없는 고객"));
+        account.setCustomer(customer);
+        account.setBalance(accountDTO.getBalance());
+        account.setCreatedAt(accountDTO.getCreatedAt());
+
+        Account saveAccount = accountRepository.save(account);
+        return convertToDTO(saveAccount);
+    }
+
+    public AccountDTO convertToDTO(Account account) {
+        AccountDTO accountDTO = new AccountDTO();
+        accountDTO.setAccountId(account.getAccountId());
+        accountDTO.setCustomerId(account.getCustomer().getCustomerId());
+        accountDTO.setBalance(account.getBalance());
+        accountDTO.setCreatedAt(account.getCreatedAt());
+        return accountDTO;
+    }
+}
