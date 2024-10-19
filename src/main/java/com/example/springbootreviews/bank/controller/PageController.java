785dd882902e58fa398faffbe52c7369f082782f
Sang Won Lee <freerusya@naver.com>
2024-10-19 20:06:15 +0900

    refactor:

--- src/main/java/com/example/springbootreviews/bank/controller/PageController.java
+++ src/main/java/com/example/springbootreviews/bank/controller/PageController.java
@@ -1,32 +1,50 @@
 package com.example.springbootreviews.bank.controller;
 
+import com.example.springbootreviews.bank.entity.Account;
 import com.example.springbootreviews.bank.entity.Customer;
-import com.example.springbootreviews.bank.service.AccountService;
-import com.example.springbootreviews.bank.service.CustomerService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping
 public class PageController {
-    private final CustomerService customerService;
-    private final AccountService accountService;
-
-    public PageController(CustomerService customerService, AccountService accountService) {
-        this.customerService = customerService;
-        this.accountService = accountService;
-    }
 
     @GetMapping("/bank")
     public String showBank() {
         return "bank/bank";
     }
 
+    @GetMapping("/bank/login")
+    public String showLoginFrom() {
+        return "bank/login";
+    }
+
+    @GetMapping("/bank/menu")
+    public String showMenuForm() {
+        return "bank/menuForm";
+    }
+
     @GetMapping("/customers/new")
     public String showCustomerFrom(Model model) {
         model.addAttribute("customer", new Customer());
         return "bank/customerForm";
+    }
+
+    @GetMapping("/accounts/new")
+    public String showAccountFrom(Model model) {
+        model.addAttribute("account", new Account());
+        return "bank/accountForm";
+    }
+
+    @GetMapping("/customers/find")
+    public String showCustomerDetails(Model model) {
+        model.addAttribute("customer", new Customer());
+        return "bank/customerDetails";
+    }
+
+    @GetMapping("/accounts/find")
+    public String showAccountDetails(Model model) {
+        model.addAttribute("account", new Account());
+        return "bank/accountDetails";
     }
 }
